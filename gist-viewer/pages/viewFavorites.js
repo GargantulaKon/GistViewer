27cf004961df6b94f3f17249d3dd23483b9e1e61
quayron <quayron@quayron.com>
2021-05-22 09:29:33 -0400

    feat(ViewGist): view favorites list

--- gist-viewer/pages/viewFavorites.js
+++ gist-viewer/pages/viewFavorites.js
@@ -1,13 +1,42 @@
 import Layout from "../components/layout";
 import common from '../styles/common.module.css';
+import Link from "next/link";
+import {useEffect, useState} from "react";
+import {setStateViaAPI} from "../library/graphQLHelper";
 
 const ViewFavorites = () => {
+    const [favoriteGistsFetch, setFavoriteGistsFetch] = useState({gists: [], isFetching: false})
+
+    useEffect(async () => {
+        try {
+            const fetchAllFavoriteGists = async () => {
+                await setStateViaAPI(favoriteGistsFetch, setFavoriteGistsFetch, 'gists',
+                    '{ getAllFavoriteGists { gistId } }', 'getAllFavoriteGists')
+            }
+            await fetchAllFavoriteGists()
+        } catch (error) {
+            console.error(error)
+        }
+    }, []);
+
     return (
         <Layout pageTitle="View Favorites)">
             <div className={common.card}>
                 <div className={common.mainInfo}>
-                    <h1> View Favorites </h1>
+                    <h1 className={common.pageTitle}> View Favorites </h1>
                 </div>
+            </div>
+            <div>
+                {!favoriteGistsFetch.isFetching ? favoriteGistsFetch.gists.map(({gistId}, i) => (
+                    <div key={gistId} className={common.gistCard}>
+                        <Link href={`/gist/${gistId}`}>
+                            <a>
+                                <div key={`id${i}`} className={common.fieldTitle}><span
+                                    className={common.fieldLabel}>ID:</span> {gistId}</div>
+                            </a>
+                        </Link>
+                    </div>
+                )) : null}
             </div>
         </Layout>
     )
