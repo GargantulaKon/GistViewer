0b7b043437ea9ba18df5371c832b0f88f0ca17f4
quayron <quayron@quayron.com>
2021-05-29 19:27:00 -0400

    fix(GUI): align star with ID link

--- gist-viewer/pages/index.js
+++ gist-viewer/pages/index.js
@@ -112,23 +112,24 @@ const Index = () => {
                     {!gistsByUser.gists.length ? 'Please search first.' : null}
                     {!gistsByUser.isFetching ? gistsByUser.gists.map(({id, description, created_at, url}, i) => (
                         <div key={id} className={common.gistCard}>
-                            <div>
+                            <div className={styles.starAndIdWrapper}>
                                 {favorites.includes(id) ?
                                     <a onClick={(event) => removeFavorite(event, id, `mutation { removeFavoriteGist(id: "${id}") { gistId } }`)}><img
-                                        alt="full star" title="Remove Favorite" src="../../image/star_full.png"/>
+                                        alt="full star" title="Remove Favorite" src="image/star_full.png"/>
                                     </a>
                                     :
                                     <a onClick={(event) => addFavorite(event, id, `mutation { setFavoriteGist(id: "${id}") { gistId } }`)}>
-                                        <img alt="empty star" title="Mark Favorite" src="../../image/star_empty.png"/>
+                                        <img alt="empty star" title="Mark Favorite"
+                                             src="image/star_empty.png"/>
                                     </a>
                                 }
+                                <Link href={`/gist/${id}`}>
+                                    <a>
+                                        <div key={`id${i}`} className={common.fieldTitle}><span
+                                            className={common.fieldLabel}>ID:</span> {id}</div>
+                                    </a>
+                                </Link>
                             </div>
-                            <Link href={`/gist/${id}`}>
-                                <a>
-                                    <div key={`id${i}`} className={common.fieldTitle}><span
-                                        className={common.fieldLabel}>ID:</span> {id}</div>
-                                </a>
-                            </Link>
                             <div key={`description${i}`} className={common.fieldRows}><span
                                 className={common.fieldLabel}>Description:</span> {description ? description : 'None'}
                             </div>
